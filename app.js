ff2538af9db328d1eb38568a3940cd32dc7d14de
parsa mahmoudy <43126637+parsa011@users.noreply.github.com>
2020-11-14 10:01:43 +0330

    edit code editors and some other changes

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
-var textAreas = document.querySelectorAll('textarea');
+var textAreas = document.querySelectorAll('.code');
 var typingTimer; //timer identifier
 var doneTypingInterval = 1000; //time in ms, 5 second for example
-var lastChar; // the last typed character
+//var lastChar; // the last typed character
+var withTwinCharacter = [16,219,57,222];
 textAreas.forEach(element => {
     element.addEventListener('keydown', e => {
         // use tab for add a \t
@@ -10,35 +11,48 @@ textAreas.forEach(element => {
             e.preventDefault();
             return false;
         }
+        
     });
     element.addEventListener('keyup', e => {
-        var start = element.selectionStart;
-        var lastChar = String.fromCharCode(element.value.charCodeAt(start - 1));
-        console.log(lastChar);
         
-        if(e.key == lastChar){
-            if (lastChar == '{') {
-                addTwink(element,"}")
-            }
-            if (lastChar == '[') {
-                addTwink(element,"]")
-            }
-            if (lastChar == '<') {
-                addTwink(element,">")
-            }
-            if (lastChar == '"') {
-                addTwink(element,'"')
-            }
-            if (lastChar == "'") {
-                addTwink(element,"'")
-            }
-            if (lastChar == "(") {
-                addTwink(element,")")
-            }
-        }else{
-            lastChar = e.Key;
-        }
+        var start = document.getSelection().anchorOffset;
         
+        // if(withTwinCharacter.includes(e.keyCode) && !e.shiftKey)
+        // {
+        //     if (e.key == '{') {
+        //         addTwins(element,"}");
+        //     }
+        //     if (e.key == '[') {
+        //         addTwins(element,"]");
+        //     }
+        //     if (e.key == '<') {
+        //         addTwins(element,">");
+        //     }
+        //     if (e.key == '"') {
+        //         addTwins(element,'"');
+        //     }
+        //     if (e.key == "'") {
+        //         addTwins(element,"'");
+        //     }
+        //     if (e.key == "(") {
+        //         addTwins(element,")");
+        //     }
+          
+        //     var range = document.createRange();
+        //     range.selectNode(element);
+        //     range.setStart(element, start);
+        //     range.setEnd(element, start);
+           
+        //     var sel = window.getSelection();
+        //     sel.removeAllRanges();
+        //     sel.addRange(range);
+
+        // }
+
+        //lastChar = e.Key;
+        element.innerHTML.replace("<div><br></div>",'\n');
+        element.innerHTML.replace("</br>",'\n');
+       
         clearTimeout(typingTimer);
         typingTimer = setTimeout(compile, doneTypingInterval);
     });
@@ -55,24 +69,25 @@ function compile() {
 
     code.open();
     code.writeln(
-        html.value +
+        html.textContent +
         "<style>" +
-        css.value +
+        css.textContent +
         "</style>"
     );
     if (js.value != lastScript) {
         code.writeln("<script>" +
-            js.value +
+            js.textContent +
             "</script>");
-        lastScript = js.value;
+        lastScript = js.textContent;
     }
     code.close();
 }
 
-function addTwink(element,value){    
-    var startPos = element.selectionStart;
-            var endPos = element.selectionEnd;
-            element.value = element.value.substring(0, startPos) +
-                value +
-                element.value.substring(endPos, element.value.length);
+// add twink of a 
+function addTwins(element,value){    
+    var startPos = document.getSelection().anchorOffset;
+    var endPos = document.getSelection().anchorOffset;
+    element.textContent = element.textContent.substring(0, startPos) +
+        value +
+        element.textContent.substring(endPos, element.textContent.length);
 }
